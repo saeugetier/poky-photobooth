4eb8551077b7dbbf5dc531ffb0e3975873dade0c
saeugetier <saeugetier@googlemail.com>
2018-08-18 10:19:43 +0200

    removing CUPS configuration from project

--- meta-photobooth/recipes-app/printerinit/files/printerinit.sh
+++
@@ -1,8 +0,0 @@
-# Check to see if the printer is already installed
-if [ `lpstat -p 2>&1 | grep -E 'CP910' -c || true` = "0" ]
-then
-    echo "Installing CP910"
-    lpadmin -p CP910 -E -m /usr/share/cups/ppd/Canon_CP910.ppd -v usb://Canon/CP910?serial=C914010200000141
-    lpadmin -d CP910
-fi
-
